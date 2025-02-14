efd1245aeb24452241e15e6863c95c1470dda48a
Izabelly Gutierres Silva <izabellygutierressilva@email.com>
2025-02-14 08:13:27 -0300

    Aplicação de lógica de login e cadastro e Componente AuthContext

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,25 +1,33 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+    const navigate = useNavigate();
+    const { handleLogout } = useContext(AuthContext);
+
+    function logout() {
+        handleLogout();
+        alert('O Usuário foi desconectado com sucesso!');
+        navigate('/');
+    }
+    
     return (
         <>
-            <div className='w-full flex justify-center py-4
-            			   bg-rose-400 text-white'>
-            
+            <div className='w-full bg-rose-400 text-white flex justify-center py-4'>
                 <div className="container flex justify-between text-lg">
-                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
                     <div className='flex gap-4'>
-                        Postagens
-                        Temas
-                        Cadastrar tema
-                        Perfil
-                        Sair
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastro-tema' className='hover:underline'>Cadastrar tema</Link>
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
