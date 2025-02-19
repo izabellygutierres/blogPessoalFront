1b4d167d3ee5caa391ab2a21984f4523d80ec906
Izabelly Gutierres Silva <izabellygutierressilva@email.com>
2025-02-18 21:49:05 -0300

    Aplicação de logica de postagem, tema, login e deletar tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,33 +1,39 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
     const navigate = useNavigate();
-    const { handleLogout } = useContext(AuthContext);
+
+    const { handleLogout } = useContext(AuthContext)
 
     function logout() {
-        handleLogout();
-        alert('O Usuário foi desconectado com sucesso!');
-        navigate('/');
+
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
     }
     
     return (
         <>
-            <div className='w-full bg-rose-400 text-white flex justify-center py-4'>
+            <div className='w-full bg-rose-900 text-white
+                flex justify-center py-4'>
+
                 <div className="container flex justify-between text-lg">
                     <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+
                     <div className='flex gap-4'>
-                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        Postagens
                         <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastro-tema' className='hover:underline'>Cadastrar tema</Link>
-                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
+                        <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+                        Perfil
                         <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
                 </div>
             </div>
         </>
-    );
+    )
 }
 
-export default Navbar;
+export default Navbar
